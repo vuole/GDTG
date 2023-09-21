1a66b68efe6b0a0c0339034c441952161cb2f473
Vu Le Van <vuole.it@gmail.com>
2023-09-21 17:02:18 +0700

    add transaction history page

--- src/components/TransactionHistory/THTopBar.tsx
+++ src/components/TransactionHistory/THTopBar.tsx
@@ -1,5 +1,71 @@
-const THTopBar = () => {
-    return <div>Topbar</div>
-}
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
 
-export default THTopBar;
+export default function THTopBar() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton size="large" color="inherit" sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Logo
+          </Typography>
+          <div>
+            <IconButton color="inherit" sx={{marginRight: "-10px"}}>
+              <div style={{fontSize: "14px"}}>0 VNĐ</div>
+            </IconButton>
+            <IconButton size="large" color="inherit">
+              <AccountBalanceWalletIcon />
+            </IconButton>
+            <IconButton size="large" color="inherit">
+              <NotificationsNoneIcon />
+            </IconButton>
+            <IconButton size="large" onClick={handleMenu} color="inherit">
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Thông tin tài khoản</MenuItem>
+              <MenuItem onClick={handleClose}>Đăng xuất</MenuItem>
+            </Menu>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
--- src/components/TransactionHistory/TransactionHistory.tsx
+++ src/components/TransactionHistory/TransactionHistory.tsx
@@ -1,14 +1,18 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import * as React from "react";
+import { styled as styledMUI } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import styled from "styled-components";
+import Button from "@mui/material/Button";
+import TablePagination from "@mui/material/TablePagination";
+import TextField from "@mui/material/TextField";
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
+const StyledTableCell = styledMUI(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.common.black,
     color: theme.palette.common.white,
@@ -18,61 +22,107 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   },
 }));
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
+const StyledTableRow = styledMUI(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
   },
   // hide last border
-  '&:last-child td, &:last-child th': {
+  "&:last-child td, &:last-child th": {
     border: 0,
   },
 }));
 
+const Title = styled.div`
+  font-size: 20px;
+  margin: 20px 10px 10px 10px;
+`;
+
 function createData(
+  date: string,
   name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
+  amount: number,
+  status: string
 ) {
-  return { name, calories, fat, carbs, protein };
+  return { date, name, amount, status };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData("21/9/2023", "Commercial Website", 1000000, "Completed"),
+  createData("21/9/2023", "Commercial Website", 1000000, "Completed"),
+  createData("21/9/2023", "Commercial Website", 1000000, "Completed"),
+  createData("21/9/2023", "Commercial Website", 1000000, "Completed"),
+  createData("21/9/2023", "Commercial Website", 1000000, "Completed"),
 ];
 
 export default function TransactionHistory() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {row.name}
+    <>
+      <Title>Lịch sử giao dịch</Title>
+      <TextField
+        variant="outlined"
+        size="small"
+        sx={{ width: "20%", margin: "0 0 5px 5px" }}
+        placeholder="Tìm kiếm..."
+      />
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Thời gian tạo</StyledTableCell>
+              <StyledTableCell align="right">Tên giao dịch</StyledTableCell>
+              <StyledTableCell align="right">Số tiền giao dịch</StyledTableCell>
+              <StyledTableCell align="right">
+                Trạng thái giao dịch
               </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+              <StyledTableCell align="right"></StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <StyledTableRow key={row.name}>
+                <StyledTableCell component="th" scope="row">
+                  {row.date}
+                </StyledTableCell>
+                <StyledTableCell align="right">{row.name}</StyledTableCell>
+                <StyledTableCell align="right">{row.amount}</StyledTableCell>
+                <StyledTableCell align="right">{row.status}</StyledTableCell>
+                <StyledTableCell align="right">
+                  <Button
+                    variant="contained"
+                    size="small"
+                    sx={{ textTransform: "unset" }}
+                  >
+                    Chi tiết
+                  </Button>
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </>
   );
 }
