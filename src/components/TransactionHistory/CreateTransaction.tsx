d8f2b05347d8025a66b9a1519a9f31fcf56e7ed6
Vu Le Van <vuole.it@gmail.com>
2023-09-27 17:33:17 +0700

    update delete chip in search box

--- src/components/TransactionHistory/CreateTransaction.tsx
+++ src/components/TransactionHistory/CreateTransaction.tsx
@@ -26,6 +26,7 @@ export default function CreateTransaction(props: CreateTransactionProps) {
   const [searchResult, setSearchResult] = useState<Array<User>>([]);
   const [name, setName] = useState<string>("");
   const [adminB, setAdminB] = useState<User>({});
+  const [isShowChip, setIsShowChip] = useState(true);
 
   const searchQuery = useDebounce(keyword, 1000);
   const isErrorForm = useMemo(() => {
@@ -53,6 +54,7 @@ export default function CreateTransaction(props: CreateTransactionProps) {
   };
 
   const handleSelectedItem = (value: any) => {
+    setIsShowChip(true);
     setAdminB(value);
     setKeyword("");
   };
@@ -79,15 +81,24 @@ export default function CreateTransaction(props: CreateTransactionProps) {
           value={keyword}
           onChange={(e) => setKeyword(e.target.value)}
           InputProps={{
-            startAdornment:
-              Object.keys(adminB).length > 0 ? (
-                <InputAdornment position="start">
-                  <Chip avatar={<Avatar>A</Avatar>} label={adminB.name} />
-                </InputAdornment>
-              ) : undefined,
+            startAdornment: Object.keys(adminB).length > 0 && isShowChip && (
+              <InputAdornment position="start">
+                <Chip
+                  avatar={<Avatar>A</Avatar>}
+                  label={adminB.name}
+                  onDelete={() => {
+                    setIsShowChip(false);
+                    setAdminB({});
+                  }}
+                />
+              </InputAdornment>
+            ),
           }}
         />
-        <SearchResultList data={searchResult} onSlectedItem={handleSelectedItem} />
+        <SearchResultList
+          data={searchResult}
+          onSlectedItem={handleSelectedItem}
+        />
       </Wrapper>
     </SFormDialog>
   );
